d7435254346dbb4b6e44265164a64f1db7f9eefe
Rene Roost <roost.rene@gmail.com>
2019-10-22 12:15:05 +0300

    Inversion of Control kasutamine trennivalikul.

---
+++ src/ee/sport/treening/JalgpalliTreener.java
@@ -0,0 +1,10 @@
+package ee.sport.treening;
+
+public class JalgpalliTreener implements Treener {
+	
+	@Override
+	public String saaIgapaevaneTrenn() {
+		return "Soorita 120 tsenderdust viiekasti";
+	}
+
+}
---
+++ src/ee/sport/treening/TereSpringApp.java
@@ -0,0 +1,35 @@
+package ee.sport.treening;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TereSpringApp {
+	
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext kontekst = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		Treener treener =  null;
+		
+		System.out.println("Vali meeldiv trenn:\n1 - pesapall\n2 - kergejoustik\n3 - jalgpall");
+		Scanner klaviatuur = new Scanner(System.in);
+		int valik = klaviatuur.nextInt();
+		
+		switch(valik) {
+		case 1:
+			treener = kontekst.getBean("pesapalliTreener", Treener.class);
+			break;
+		case 2:
+			treener = kontekst.getBean("kergejoustikuTreener", Treener.class);
+			break;
+		case 3:
+			treener = kontekst.getBean("jalgpalliTreener", Treener.class);
+			break;
+		}
+		
+		System.out.println(treener.saaIgapaevaneTrenn());
+		kontekst.close();
+	}
+
+}
