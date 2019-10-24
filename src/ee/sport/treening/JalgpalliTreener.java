78d648015e884779a0485bf1bea0bc55475983e1
Rene Roost <roost.rene@gmail.com>
2019-10-24 14:12:51 +0300

    Dependency Injection kasutamine setterites kasutades otseseid väärtusi ja väärtusi failist.

--- src/ee/sport/treening/JalgpalliTreener.java
+++ src/ee/sport/treening/JalgpalliTreener.java
@@ -4,10 +4,29 @@ public class JalgpalliTreener implements Treener {
 	
 	public EttekuulutusTeenus ettekuulutusTeenus;
 	
+	private String emailiAadress;
+	private String meeskond;
+	
 	public JalgpalliTreener() {
 		// System.out.println("JalgpalliTreener: ilma argumentideta konstruktori sees");
 	}
 	
+	public String getEmailiAadress() {
+		return emailiAadress;
+	}
+
+	public void setEmailiAadress(String emailiAadress) {
+		this.emailiAadress = emailiAadress;
+	}
+
+	public String getMeeskond() {
+		return meeskond;
+	}
+
+	public void setMeeskond(String meeskond) {
+		this.meeskond = meeskond;
+	}
+
 	public void setEttekuulutusTeenus(EttekuulutusTeenus ettekuulutusTeenus) {
 		// System.out.println("JalgpalliTreener: setteri meetodi sees - setEttekuulutusTeenus");
 		this.ettekuulutusTeenus = ettekuulutusTeenus;
